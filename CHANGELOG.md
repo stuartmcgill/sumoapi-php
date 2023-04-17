96113c9ee6394174955668a100356d5ddcd9610c
Stuart McGill <stuartmcgill@capuk.org>
2023-04-17 17:43:11 +0100

    Add CHANGELOG, config, and `Rikishi` model

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+
+## [0.0.1] - 2023-04-17
+
+### Added
+- Initial version of the API
+
+### Changed
+
+### Fixed
---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'divisions' => ['Makuuchi', 'Juryo', 'Makushita', 'Sandanme', 'Jonidan', 'Jonokuchi'],
+    'apiRateLimit' => 500,
+];
